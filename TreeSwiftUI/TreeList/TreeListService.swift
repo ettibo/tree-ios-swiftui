e1e1c4c0a7bbec9b96dc97cfc2bc7958ffe8d072
Thibaud HUCHON <t.huchon@LGOP3594.local>
2022-01-21 17:00:21 +0100

    remvove commented code

--- TreeSwiftUI/TreeList/TreeListService.swift
+++ TreeSwiftUI/TreeList/TreeListService.swift
@@ -30,19 +30,5 @@ class TreeListService {
                 completion(.failure(error))
             }
         }
-//
-//        guard let url = URL(string: "https://opendata.paris.fr/api/records/1.0/search/?dataset=les-arbres&rows=20") else {
-//            print("Invalid URL")
-//            return
-//        }
-//        do {
-//            let (data, _) = try await URLSession.shared.data(from: url)
-//            if let decodedResponse = try? JSONDecoder().decode(Welcome.self, from: data) {
-//                completionHandler(decodedResponse.records)
-//            }
-//            // more code to come
-//        } catch {
-//            print("Invalid data")
-//        }
     }
 }
